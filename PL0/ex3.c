e82bc03d4e3be35a1181c1fa9832b3fdbb9baadd
Tiago Ribeiro <1181444@isep.ipp.pt>
2020-02-14 15:52:11 +0000

    ex3

--- PL0/ex3.c
+++ PL0/ex3.c
@@ -5,27 +5,24 @@ uma função que recebendo como parâmetros o endereço de um vector de inteiros
 e o seu número de elementos, devolva a soma dos elementos pares do vector.
 */
 int sum = 0;
-int array[] = {23,5,46,12,1,3,10,7,26,94};
-int numElements = sizeof(array)/sizeof(array[0]);
 
 int main(){
+	int array[] = {23,5,46,12,1,3,10,7,26,94};
+	int numElements = sizeof(array)/sizeof(array[0]);	
+	sumEven(array, numElements);
 	
-	int *ptr = array;
-	int sum = 0, i = 0;
-
-	sumEven(array, i);
 	return 0;	
 }
 
 void sumEven(int *num, int i){
 	
-	if(i == 10){										//if no more elements to iterate, print the sum value
+	if(i == 0){											//if no more elements to iterate, print the sum value
 		printf("Sum of even elements = %d\n", sum);
 	}
 	if(*(num+i) % 2 == 0){								//if element is pair, add it to sum variable
 		sum += *(num+i);
 	}
-	sumEven(num, i+1);									//recursive call, passing (array pointer, next iteration of array)	
+	sumEven(num, i-1);									//recursive call, passing (array pointer, next iteration of array)	
 }
 
 
