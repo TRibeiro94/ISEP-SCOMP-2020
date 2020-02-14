73477a874e586fdee988439b89536c19ce8237c2
Tiago Ribeiro <1181444@isep.ipp.pt>
2020-02-14 16:49:19 +0000

    ex4

---
+++ PL0/ex4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/*
+4. Resolver recursivamente: Usando aritmética de apontadores, implemente uma função 
+que recebendo como parâmetros o endereço de um vector de inteiros, o seu número de 
+elementos e um determinado valor, subtraia esse valor a todos os elementos do vector.
+*/
+int array[] = {10,15,20,25,30,35,40,45,50,55};
+int num = sizeof(array)/sizeof(array[0]);
+
+int main(){
+	int target;
+	
+	printf("Insert a value to be subtracted to all elements:\n");
+	scanf("%d", &target);
+	subValueToAllElements(array, num, target);
+	
+	return 0;
+}
+
+void subValueToAllElements(int *array, int numElements, int target){
+	int j;
+	numElements--;				
+	
+	if(numElements == -1){
+		printf("[ ");
+		for(j = 0; j < num; j++){
+			if(j == num - 1){								
+				printf("%d ]", *(array+j));	
+			}else{
+				printf("%d, ", *(array+j));
+			}
+		}
+    }else{
+    	*(array+numElements) -= target;
+	}
+	subValueToAllElements(array, numElements, target);												
+}
+
+
+
