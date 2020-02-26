485ecdfae89c079e501d027b138683cbb51f8d03
Tiago Ribeiro <1181444@isep.ipp.pt>
2020-02-26 22:17:20 +0000

    ex6

--- PL1/ex6/main.c
+++ PL1/ex6/main.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>	
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main (){
 	
     int i;
-
+	printf("\n");
     for (i = 0; i < 4; i++){
         if (fork () == 0) {
-            sleep (1);	
+			printf("(son) pid %d from (parent) pid %d.\n",getpid(), getppid());
+            exit(0);	
         }
     }
+    for(i = 0; i < 4; i++){
+		wait(NULL);
+	} 
+    
     printf ("This is the end.\n");
     return 0;
 }
@@ -17,9 +25,9 @@ int main (){
 /*
  * a)
  * Q: How many processes will be created by this code? Justify by drawing a process tree that describes the processes created.
- * A: 2⁴ = 16 (.png at ex6 folder)
+ * A: 2^4 = 16 (.png at ex6 folder)
  * 
  * b)
  * Q: What change, if any, would you do to this code so that exactly 4 child processes are created?
- * A: Change the '< 4' at the for cycle for '< 2'.
+ * A: Exchange sleep(1) for exit(0) inside the for loop, and add a loop below as seen above to wait all child processes to finish.
  */
