5d83d78cff885fd70492e318cd933affd1b11d3d
Tiago Ribeiro <1181444@isep.ipp.pt>
2020-02-27 10:03:31 +0000

    ex6 C

--- PL1/ex6/point_C/pointC.c
+++ PL1/ex6/point_C/pointC.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>	
 #include <sys/types.h>
 #include <sys/wait.h>
 
 int main (){
 	
-    int i;
-    int status;
-    pid_t p;
+    int i, status;
+    pid_t arrayPids[4];
 
-    for (i = 0; i < 2; i++){
-		p = fork();
-        
-        if (p == 0) {
-			printf("Sleeping for 1 second.\n");
-            sleep (1);	
-        }else{
-			if(p % 2 == 0){
-				printf("Waiting for child process with even PID = %d\n", p);
-				waitpid(p, &status, 0);
-			}	
-		}
+    for (i = 0; i < 4; i++){
+        if((arrayPids[i] = fork ()) == 0) {
+			printf("(son) pid %d from (parent) pid %d.\n", getpid(), getppid());
+            exit(0);	
+        }
     }
+    for(i = 0; i < 4; i++){
+		if(arrayPids[i] % 2 == 0){
+			printf("Waiting for child process with even PID (%d)\n", arrayPids[i]);
+			waitpid(arrayPids[i], &status, 0);
+		}
+	}
     
     printf ("This is the end.\n");
     return 0;
